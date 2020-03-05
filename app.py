1e457bfec4b6b9c97f654ce266ae450cc157f6df
Carlos Cabello Valdes <carloscabello364@gmail.com>
2020-03-04 19:52:54 -0600

    Added test endpoint

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response, request, render_template, Response
 
 app = Flask(__name__)
 
 @app.route('/')
 def register():
-    return 'servidorino funcionarin'
+    return 'servidorino funcionarino'
+
+@app.route('/upload', methods=['GET'])
+def getImageLink():
+    return 'Imagen bien chida'
+
+@app.route('/upload', methods=['POST'])
+def postImageLink():
+    req       = request.json
+    test      = req.get("test")
+    print(test)
+    return test
 
 app.run('0.0.0.0', '8080', debug=True)
