278caaba8cad3cfafad5cfae8abf7c50f97a1fac
Juan Carlos Quirino <juanquirinoc@gmail.com>
2020-03-12 19:20:30 -0600

    Modified models

--- models.py
+++ models.py
@@ -1,4 +1,8 @@
-from mongoengine import Document, StringField
+from mongoengine import Document, StringField, ListField
 
 class Link(Document):
   Link = StringField(required=True, max_length=200)
+
+class Post(Document):
+  Title = StringField(required=True)
+  RelatedTo = ListField(required=True)
