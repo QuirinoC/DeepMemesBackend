f5edc93f3a28cd4527c32b908acd25758020ba05
Juan Carlos Quirino <juanquirinoc@gmail.com>
2020-04-15 11:21:45 -0600

    Restructured project folder structure

--- models.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import Document, StringField, ListField
-
-class Link(Document):
-  Link = StringField(required=True, max_length=200)
-
-class Post(Document):
-  Title = StringField(required=True)
-  RelatedTo = ListField(required=True)
