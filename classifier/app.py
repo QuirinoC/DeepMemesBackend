82dd381b1d1f1ed385e2bf3466c50333c9350198
Juan Carlos Quirino <juanquirinoc@gmail.com>
2020-04-15 10:34:30 -0600

    Added classifier service

---
+++ classifier/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, make_response, request, render_template, Response
+
+from mongoengine import *
+import os
+
+from flask_cors import CORS, cross_origin
+
+connection = connect('deep_memes_database',
+        host='deep_memes_database',
+        port=27017
+        )
+#connect('deep_memes_database')
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+class Submission(Document):
+    link = StringField(required=True)
+    tags = ListField(StringField(max_length=30))
+
+@app.route('/')
+def register():
+    return jsonify('Classifier API')
+ 
+app.run('0.0.0.0', '8080', debug=True)
