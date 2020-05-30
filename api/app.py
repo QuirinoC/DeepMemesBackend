d5b37af79a21fec8b6a3db90e8fe1898af77a9b7
ricardope3 <ricky.foals@gmail.com>
2020-05-30 16:28:30 -0500

    quirino hazme el amor

--- api/app.py
+++ api/app.py
@@ -17,6 +17,8 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 # MODELS
+
+
 class Meme(Document):
     link = StringField(required=True)
     tags = ListField(StringField(max_length=30))
@@ -26,15 +28,18 @@ class Meme(Document):
     likes = IntField(required=True)
     dislikes = IntField(required=True)
 
+
 class User(Document):
     username = StringField(required=True)
     profilePictureLink = StringField(required=True)
     tags = ListField(StringField(max_length=30))
     email = StringField(required=True)
 
+
 @app.route('/',  methods=['GET'])
 def register():
     return 'Servidorino APIrino'
+
 
 @app.route('/getUser',  methods=['GET'])
 def getUser():
@@ -46,6 +51,7 @@ def getUser():
     res['profilePictureLink'] = user.profilePictureLink
     res['tags'] = user.tags
     return jsonify(res)
+
 
 @app.route('/getRelatedTo')
 def memeRelatedTo():
@@ -60,11 +66,20 @@ def memeRelatedTo():
         for tag in meme.tags:
             for query in queries:
                 if tag == query and count < resLimit:
-                    res.append(meme.link)
-                    count+=1
+                    res.append({"link": meme.link, "title": meme.title, "email": meme.email,
+                                "likes": meme.likes, "dislikes": meme.dislikes, "comments": meme.comments, "id": meme.id, })
+                    count += 1
                 if count >= resLimit:
                     break
     return jsonify(res)
+    # link = StringField(required=True)
+    # tags = ListField(StringField(max_length=30))
+    # title = StringField(required=True)
+    # email = StringField(required=True)
+    # comments = ListField(StringField(max_length=300))
+    # likes = IntField(required=True)
+    # dislikes = IntField(required=True)
+
 
 @app.route('/getRandom')
 def memeRandom():
@@ -78,19 +93,22 @@ def memeRandom():
     while count < resLimit:
         if count >= len(Meme.objects):
             break
-        target = random.randrange(0,len(Meme.objects), 1)
+        target = random.randrange(0, len(Meme.objects), 1)
         if target not in burnt:
             count += 1
             burnt.append(target)
-            res.append(Meme.objects[target].link)
-    print(res)
+
+            res.append({"link": Meme.objects[target].link, "title": Meme.objects[target].title, "tags": Meme.objects[target].tags, "email": Meme.objects[target].email,
+                        "comments": Meme.objects[target].comments, "likes": Meme.objects[target].likes, "dislikes": Meme.objects[target].dislikes, "id": str(Meme.objects[target].id)})
     return jsonify(res)
+
 
 @app.route('/getComments')
 def getComments():
     targetMeme = request.args.get('memeUid')
     meme = Meme.objects.get(id=targetMeme)
     return jsonify(meme.comments)
+
 
 @app.route('/getReactions')
 def getReactions():
@@ -101,39 +119,42 @@ def getReactions():
     res['dislikes'] = meme.dislikes
     return jsonify(res)
 
+
 @app.route('/createUser', methods=['POST'])
 def createUser():
-    req       = request.json
-    user      = User(
-        username           = req["username"],
-        email              = req["email"],
-        profilePictureLink = req["profilePictureLink"],
+    req = request.json
+    user = User(
+        username=req["username"],
+        email=req["email"],
+        profilePictureLink=req["profilePictureLink"],
     )
     user.save()
     return make_response(
-    "<h1>" + 
-            "username: "            + user.username  + "<br>" +
-            "email: "               + user.email  + "<br>" +
-            "profilePictureLink: "  + user.profilePictureLink  + "<br>" +
-    "</h1>")
+        "<h1>" +
+        "username: " + user.username + "<br>" +
+        "email: " + user.email + "<br>" +
+        "profilePictureLink: " + user.profilePictureLink + "<br>" +
+        "</h1>")
+
 
 @app.route('/createMeme', methods=['POST'])
 def createMeme():
-    req       = request.json
-    meme      = Meme(
-        link     = req["link"],
-        title    = req["title"],
-        email   = req["email"],
-        likes    = 0,
-        dislikes = 0
+    req = request.json
+    meme = Meme(
+        link=req["link"],
+        title=req["title"],
+        email=req["email"],
+        likes=0,
+        dislikes=0
     )
     meme.save()
     return make_response(
-    "<h1>" + 
-            "email: " + meme.email + "<br>" +
-            "Link: "   + meme.link  + "<br>" +
-            "Title: "  + meme.title + "<br>" +
-    "</h1>")
+        "<h1>" +
+        "email: " + meme.email + "<br>" +
+        "Link: " + meme.link + "<br>" +
+        "Title: " + meme.title + "<br>" +
+        "</h1>")
+
 
 @app.route('/createComment', methods=['POST'])
 def createComment():
@@ -146,26 +167,42 @@ def createComment():
     meme.save()
     return make_response("Success")
 
+
+@app.route('/updateUserTags', methods=['POST'])
+def updateUserTags():
+    req = request.json
+    targetUser = req.get("email")
+    tag = req.get("tags")
+    print(targetUser)
+    user = User.objects.get(email=targetUser)
+    if(not(tag in user.tags)):
+        print(user.tags)
+        # user.tags.append(tag)
+    user.save()
+    return make_response("Success")
+
+
 @app.route('/reaction', methods=['POST'])
 def reaction():
     req = request.json
     targetMeme = req.get("uidMeme")
     targetUser = req.get("email")
-    type       = req.get("type")
+    type = req.get("type")
 
     meme = Meme.objects.get(id=targetMeme)
     if type == 0:
         meme.likes = meme.likes + 1
-        user = User.objects.get(id=targetUser)
+        user = User.objects.get(email=targetUser)
         for tag in meme.tags:
             user.tags.append(tag)
         meme.save()
         user.save()
         return make_response("Liked")
-    
+
     if type == 1:
         meme.dislikes = meme.dislikes + 1
         meme.save()
         return make_response("Disliked")
- 
+
+
 app.run('0.0.0.0', '8080', debug=True)
